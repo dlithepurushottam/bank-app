ef569ac4b039d7706b5c9e50704390329b2e179f
dlithepurushottam <dlithepurushottam@gmail.com>
2025-05-21 09:41:24 +0530

    update dwith header.css file

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import {Route,Routes} from "react-router-dom";
 import PostUser from './components/postUser/postUser';
 import UpdateUser from './components/updateUser/updateUser';
 import NoMatch from './components/nomatch/noMatch';
+import Header from './components/header/header';
 function App() {
   return (
       <>
+      <Header></Header>
       <Routes>
           <Route path="/" element={<Dashboard></Dashboard>}></Route>
             <Route path="/user" element={<PostUser></PostUser>}></Route>
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,20 @@
-const Header = ( ) =>{
+import {Navbar, Nav,Container} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./header.css";
+
+const Header = () =>{
     return(
-        <h1>Header component</h1>
+        <>
+        <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="/">X Bank</Navbar.Brand>
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/" className="nav-link">Dashboard</Nav.Link>
+            <Nav.Link as={Link} to="/user" className="nav-link">PostUser</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+        </>
     );
 };
 
